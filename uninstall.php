ac464c8feae6a53f94e12ea28b327d58e208cf9e
mahabub hasan <mahabub hasan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-15 11:27:48 +0000

    mahabub hasan
    git-svn-id: https://plugins.svn.wordpress.org/service-boxes-widgets-text-icon/trunk@1135405 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ uninstall.php
@@ -0,0 +1,19 @@
+<?php
+/**
+   * Service Boxes Widgets Text Icon when the plugin is uninstalled.
+   *
+   * @package   service_boxes_widget_text_icon
+   * @author    Md. Mahabub Masan Manik <bdwebteam@gmail.com>
+   * @license   MIT License
+   * @link      http://bdwebteam.com/
+   * @copyright 2015 Mahabub Hasan
+ */
+
+// If uninstall, not called from WordPress, then exit
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}
+
+// Delete plugin settings
+delete_option( 'service_boxes_widget_text_icon' );
+delete_site_option( 'service_boxs_widget_text_icon' );
